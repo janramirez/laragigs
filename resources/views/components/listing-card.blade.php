da14faa3bf36838fb5943cd5bd53575c2a01fbc6
janramirez <jramirez.ust@gmail.com>
2023-02-28 15:14:06 +0800

    Fixed listing tags created listing-tags blade components to replace hard-coded tags. used tags from database instead of hard-coded list tags.

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -3,31 +3,18 @@
 <x-card>
 
     <div class="flex">
-        <img src="{{asset('images/no-image.png')}}" alt="" class="hidden w-48 mr-6 md:block">
+        <img src="{{ asset('images/no-image.png') }}" alt="" class="hidden w-48 mr-6 md:block">
         <div>
             <h3 class="text-2xl">
-                <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
+                <a href="/listings/{{ $listing->id }}">{{ $listing->title }}</a>
             </h3>
             <div class="text-xl font-bold mb-4">
-                {{$listing->company}}
+                {{ $listing->company }}
             </div>
-            <ul class="flex">
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Laravel</a>
-                </li>
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">API</a>
-                </li>
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Backend</a>
-                </li>
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Vue</a>
-                </li>
-            </ul>
+            <x-listing-tags :tagsCsv="$listing->tags" />
             <div class="text-lg mt-4">
                 <i class="fa-solid fa-location-dot"></i>
-                {{$listing->location}}
+                {{ $listing->location }}
             </div>
         </div>
     </div>

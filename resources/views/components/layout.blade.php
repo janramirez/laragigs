06a7cc8383c2f2218fb7d3b49cc193847048ccca
janramirez <jramirez.ust@gmail.com>
2023-02-28 20:36:32 +0800

    Created flash message/modal when successfully submitting form

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="images/favicon.ico" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" referrerpolicy="no-referrer" />
+    <script src="//unpkg.com/alpinejs" defer></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
@@ -44,6 +45,9 @@
         <p class="ml-2">Copyright &copy; 2022, All Rights Reserved</p>
         <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
     </footer>
+
+    <x-flash-message />
 </body>
 
 </html>
+

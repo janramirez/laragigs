0565c345533dbb3bfa4b80c91725e5a192b4f596
janramirez <jramirez.ust@gmail.com>
2023-03-01 09:52:35 +0800

    Updated footer as a partial
    partial._footer will show a different call-to-action when authenticated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
 
-        Listing::factory(6)->create();
+        Listing::factory(15)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,24 +1,24 @@
 <x-layout>
-    
+
     @include('partials._hero')
     @include('partials._search')
-    
+
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
-    
-        @unless (count($listings) == 0)
-    
-        @foreach ($listings as $listing)
-            <x-listing-card :listing="$listing" />
-        @endforeach
-    
+
+        @unless(count($listings) == 0)
+
+            @foreach ($listings as $listing)
+                <x-listing-card :listing="$listing" />
+            @endforeach
         @else
-        <x-card class="p-6">No listings available</x-card>
-    
+            <x-card class="p-6">No listings available</x-card>
+
         @endunless
-    
+
     </div>
     <div class="mt-6 p-4">
-        {{$listings->links()}}
+        {{ $listings->links() }}
     </div>
 
+    @include('partials._footer')
 </x-layout>
